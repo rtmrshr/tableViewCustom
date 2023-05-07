8d54af9b76e61db94436cf6d50974ffeb60821c7
Ratmir Sher <ratmir.sher@gmail.com>
2023-05-07 17:57:23 +0300

    Initial Commit

---
+++ tableViewCustom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableViewCustom
+//
+//  Created by  ratmir on 07.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
